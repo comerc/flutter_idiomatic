1241b1798507bd89da1783dab547d86e06e4b209
comerc <andrew.kachanov@gmail.com>
2020-10-23 00:18:13 +0300

    Restore tests for models

--- test/models/email_test.dart
+++ test/models/email_test.dart
@@ -1,47 +1,45 @@
-void main() {}
+// ignore_for_file: prefer_const_constructors
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_firebase_login/import.dart';
 
-// // ignore_for_file: prefer_const_constructors
-// import 'package:flutter_firebase_login/authentication/authentication.dart';
-// import 'package:flutter_test/flutter_test.dart';
+void main() {
+  const emailString = 'test@gmail.com';
+  group('Email', () {
+    group('constructors', () {
+      test('pure creates correct instance', () {
+        final email = EmailModel.pure();
+        expect(email.value, '');
+        expect(email.pure, true);
+      });
 
-// void main() {
-//   const emailString = 'test@gmail.com';
-//   group('Email', () {
-//     group('constructors', () {
-//       test('pure creates correct instance', () {
-//         final email = Email.pure();
-//         expect(email.value, '');
-//         expect(email.pure, true);
-//       });
+      test('dirty creates correct instance', () {
+        final email = EmailModel.dirty(emailString);
+        expect(email.value, emailString);
+        expect(email.pure, false);
+      });
+    });
 
-//       test('dirty creates correct instance', () {
-//         final email = Email.dirty(emailString);
-//         expect(email.value, emailString);
-//         expect(email.pure, false);
-//       });
-//     });
+    group('validator', () {
+      test('returns invalid error when email is empty', () {
+        expect(
+          EmailModel.dirty('').error,
+          EmailValidationError.invalid,
+        );
+      });
 
-//     group('validator', () {
-//       test('returns invalid error when email is empty', () {
-//         expect(
-//           Email.dirty('').error,
-//           EmailValidationError.invalid,
-//         );
-//       });
+      test('returns invalid error when email is malformed', () {
+        expect(
+          EmailModel.dirty('test').error,
+          EmailValidationError.invalid,
+        );
+      });
 
-//       test('returns invalid error when email is malformed', () {
-//         expect(
-//           Email.dirty('test').error,
-//           EmailValidationError.invalid,
-//         );
-//       });
-
-//       test('is valid when email is valid', () {
-//         expect(
-//           Email.dirty(emailString).error,
-//           isNull,
-//         );
-//       });
-//     });
-//   });
-// }
+      test('is valid when email is valid', () {
+        expect(
+          EmailModel.dirty(emailString).error,
+          isNull,
+        );
+      });
+    });
+  });
+}
