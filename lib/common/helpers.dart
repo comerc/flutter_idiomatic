2bf508bb32c7b3e48676e0f9e4f76790f7df7e1d
comerc <andrew.kachanov@gmail.com>
2020-10-29 03:41:15 +0300

    Add out()

--- lib/common/helpers.dart
+++ lib/common/helpers.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 T getBloc<T extends Cubit<Object>>(BuildContext context) =>
     BlocProvider.of<T>(context);
 
 T getRepository<T>(BuildContext context) => RepositoryProvider.of<T>(context);
+
+void out(dynamic value) {
+  if (kDebugMode) debugPrint('$value');
+}
