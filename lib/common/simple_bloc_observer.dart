3ae6eccd5ea7e8889626d39df8dad54e90ec771d
comerc <andrew.kachanov@gmail.com>
2020-11-02 23:05:54 +0300

    Remove unused callbacks

--- lib/common/simple_bloc_observer.dart
+++ lib/common/simple_bloc_observer.dart
@@ -3,12 +3,6 @@ import 'package:flutter_firebase_login/import.dart';
 
 class SimpleBlocObserver extends BlocObserver {
   @override
-  void onEvent(Bloc bloc, Object event) {
-    out(event);
-    super.onEvent(bloc, event);
-  }
-
-  @override
   void onError(Cubit cubit, Object error, StackTrace stackTrace) {
     out(error);
     super.onError(cubit, error, stackTrace);
@@ -18,11 +12,5 @@ class SimpleBlocObserver extends BlocObserver {
   void onChange(Cubit cubit, Change change) {
     out(change);
     super.onChange(cubit, change);
-  }
-
-  @override
-  void onTransition(Bloc bloc, Transition transition) {
-    out(transition);
-    super.onTransition(bloc, transition);
   }
 }
