bd3e941e774a4e004f26d567b7382ea2e37d1327
comerc <andrew.kachanov@gmail.com>
2020-12-20 07:10:44 +0300

    Move BotToastInit

--- lib/main.dart
+++ lib/main.dart
@@ -101,6 +101,7 @@ class AppView extends StatelessWidget {
       // home: TodosScreen(),
       builder: (BuildContext context, Widget child) {
         var result = child;
+        result = BotToastInit()(context, result);
         result = BlocListener<AuthenticationCubit, AuthenticationState>(
           listener: (BuildContext context, AuthenticationState state) {
             final cases = {
@@ -123,7 +124,6 @@ class AppView extends StatelessWidget {
           },
           child: result,
         );
-        result = BotToastInit()(context, result);
         return result;
       },
       onGenerateRoute: (_) => SplashScreen().getRoute(),
