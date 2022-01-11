2a30bc5d3aa2dbec9efd6dcb9e0d24b6f4823340
comerc <andrew.kachanov@gmail.com>
2022-01-11 22:41:09 +0300

    Update sign_up_test.dart

--- test/screens/sign_up_test.dart
+++ test/screens/sign_up_test.dart
@@ -3,7 +3,7 @@ import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:formz/formz.dart';
-import 'package:mockito/mockito.dart';
+import 'package:mocktail/mocktail.dart';
 import 'package:flutter_idiomatic/import.dart';
 
 class MockAuthenticationRepository extends Mock
@@ -39,16 +39,17 @@ void main() {
     });
   });
 
-  const testEmail = 'test@gmail.com';
-  const testPassword = 'testP@ssw0rd1';
-  const testConfirmedPassword = 'testP@ssw0rd1';
-
   group('SignUpForm', () {
+    const testEmail = 'test@gmail.com';
+    const testPassword = 'testP@ssw0rd1';
+    const testConfirmedPassword = 'testP@ssw0rd1';
+
     late SignUpCubit signUpCubit;
 
     setUp(() {
       signUpCubit = MockSignUpCubit();
-      when(signUpCubit.state).thenReturn(SignUpState());
+      when(() => signUpCubit.state).thenReturn(SignUpState());
+      when(() => signUpCubit.signUpFormSubmitted()).thenAnswer((_) async {});
     });
 
     group('calls', () {
@@ -64,7 +65,7 @@ void main() {
           ),
         );
         await tester.enterText(find.byKey(Key('_EmailInput')), testEmail);
-        verify(signUpCubit.doEmailChanged(testEmail)).called(1);
+        verify(() => signUpCubit.doEmailChanged(testEmail)).called(1);
       });
 
       testWidgets('doPasswordChanged when password changes', (tester) async {
@@ -79,7 +80,7 @@ void main() {
           ),
         );
         await tester.enterText(find.byKey(Key('_PasswordInput')), testPassword);
-        verify(signUpCubit.doPasswordChanged(testPassword)).called(1);
+        verify(() => signUpCubit.doPasswordChanged(testPassword)).called(1);
       });
 
       testWidgets('doConfirmedPasswordChanged when confirmedPassword changes',
@@ -95,14 +96,17 @@ void main() {
           ),
         );
         await tester.enterText(
-            find.byKey(Key('_ConfirmPasswordInput')), testConfirmedPassword);
-        verify(signUpCubit.doConfirmedPasswordChanged(testConfirmedPassword))
-            .called(1);
+          find.byKey(Key('_ConfirmPasswordInput')),
+          testConfirmedPassword,
+        );
+        verify(
+          () => signUpCubit.doConfirmedPasswordChanged(testConfirmedPassword),
+        ).called(1);
       });
 
       testWidgets('signUpFormSubmitted when sign up button is pressed',
           (tester) async {
-        when(signUpCubit.state).thenReturn(
+        when(() => signUpCubit.state).thenReturn(
           SignUpState(status: FormzStatus.valid),
         );
         await tester.pumpWidget(
@@ -116,7 +120,7 @@ void main() {
           ),
         );
         await tester.tap(find.byKey(Key('_SignUpButton')));
-        verify(signUpCubit.signUpFormSubmitted()).called(1);
+        verify(() => signUpCubit.signUpFormSubmitted()).called(1);
       });
     });
 
@@ -147,8 +151,9 @@ void main() {
       testWidgets('invalid email error text when email is invalid',
           (tester) async {
         final emailInput = MockEmailInputModel();
-        when(emailInput.invalid).thenReturn(true);
-        when(signUpCubit.state).thenReturn(SignUpState(emailInput: emailInput));
+        when(() => emailInput.invalid).thenReturn(true);
+        when(() => signUpCubit.state)
+            .thenReturn(SignUpState(emailInput: emailInput));
         await tester.pumpWidget(
           MaterialApp(
             home: Scaffold(
@@ -165,8 +170,8 @@ void main() {
       testWidgets('invalid password error text when password is invalid',
           (tester) async {
         final passwordInput = MockPasswordInputModel();
-        when(passwordInput.invalid).thenReturn(true);
-        when(signUpCubit.state)
+        when(() => passwordInput.invalid).thenReturn(true);
+        when(() => signUpCubit.state)
             .thenReturn(SignUpState(passwordInput: passwordInput));
         await tester.pumpWidget(
           MaterialApp(
@@ -185,8 +190,8 @@ void main() {
           'invalid confirmedPassword error text'
           ' when confirmedPassword is invalid', (tester) async {
         final confirmedPasswordInput = MockConfirmedPasswordInputModel();
-        when(confirmedPasswordInput.invalid).thenReturn(true);
-        when(signUpCubit.state).thenReturn(
+        when(() => confirmedPasswordInput.invalid).thenReturn(true);
+        when(() => signUpCubit.state).thenReturn(
             SignUpState(confirmedPasswordInput: confirmedPasswordInput));
         await tester.pumpWidget(
           MaterialApp(
@@ -203,7 +208,7 @@ void main() {
 
       testWidgets('disabled sign up button when status is not validated',
           (tester) async {
-        when(signUpCubit.state).thenReturn(
+        when(() => signUpCubit.state).thenReturn(
           SignUpState(status: FormzStatus.invalid),
         );
         await tester.pumpWidget(
@@ -224,7 +229,7 @@ void main() {
 
       testWidgets('enabled sign up button when status is validated',
           (tester) async {
-        when(signUpCubit.state).thenReturn(
+        when(() => signUpCubit.state).thenReturn(
           SignUpState(status: FormzStatus.valid),
         );
         await tester.pumpWidget(
@@ -241,6 +246,32 @@ void main() {
           find.byKey(Key('_SignUpButton')),
         );
         expect(signUpButton.enabled, isTrue);
+      });
+    });
+
+    group('navigates', () {
+      testWidgets('back to previous page when submission status is success',
+          (tester) async {
+        whenListen(
+          signUpCubit,
+          Stream.fromIterable(const <SignUpState>[
+            SignUpState(status: FormzStatus.submissionInProgress),
+            SignUpState(status: FormzStatus.submissionSuccess),
+          ]),
+        );
+        await tester.pumpWidget(
+          MaterialApp(
+            home: Scaffold(
+              body: BlocProvider.value(
+                value: signUpCubit,
+                child: SignUpForm(),
+              ),
+            ),
+          ),
+        );
+        expect(find.byType(SignUpForm), findsOneWidget);
+        await tester.pumpAndSettle();
+        expect(find.byType(SignUpForm), findsNothing);
       });
     });
   });
